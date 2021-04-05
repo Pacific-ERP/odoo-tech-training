a3a3851f013902976829731fb101541c29a8c47d
Pacific-ERP <info@pacific-erp.com>
2021-04-05 22:28:56 +0000

    [ADD] Basic Module

---
+++ Odoo_Space_Mission/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Space Mission',
+    
+    'summary: """App to help Odoo going on the Moon""",
+    
+    'descritpion': """
+        Module that will plan and help Odoo get to the Moon
+    """,
+    
+    'author': 'Gaëtan',
+    
+    'website': 'https://www.pacific-erp.com',
+    
+    'category': 'Space travel',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ],
+    
+    
+    
+}
