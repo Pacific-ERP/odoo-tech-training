0f7bebcec9c375628a50ac641b750591cc6267e7
Pacific-ERP <info@pacific-erp.com>
2021-04-05 23:54:02 +0000

    [ADD] Basic Module

---
+++ library_management/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Library Managmeent',
+    
+    'summary': """App tu manage a library""",
+    
+    'description': """
+        Library management module for:
+        - manage books
+        - manage customers
+        - customer checking out books
+        - custoimer checking organizing books and rental
+    """,
+    
+    'author': 'Gaëtan',
+    
+    'website': 'https://www.pacific-erp.com',
+    
+    'category': 'customers apps'
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ],
+    
+    'demo': [
+        
+        
+    ],
+}
